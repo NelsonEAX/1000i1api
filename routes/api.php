d1a4afd77797ad411dd99d014e6de68411856e95
NelsonEAX <nelsoneax@yandex.ru>
2017-02-01 20:01:57 +0500

    Попытка регистрации юзера

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,19 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group(['middleware' => ['web','guest']], function () {
+    Route::get('/csrf', function () {
+        return response()->json([
+            'csrf' => csrf_token()
+        ]);
+    });
+});
+
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
 
 Route::get('/test', function(){
    return response()->json([
@@ -27,5 +37,9 @@ Route::get('/test', function(){
 });
 
 Route::group(['middleware' => 'auth:api'], function(){
-    Route::resource('products','ProductsController');
+    
+    
+    
+    
+    Route::resource('products','ProductsController@index');
 });
